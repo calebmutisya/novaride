178330ca2e0ffa4ea58690c9aa96dd42cd8008f6
calebmutisya <mutisyacalebmusau@gmail.com>
2025-05-10 14:18:11 +0300

    structured navbar

---
+++ src/app/services/weddingservices/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Weddingservice() {
+  return (
+    <div>Weddingservice</div>
+  )
+}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import Image from 'next/image'
 import logo from "../assets/images/logo.svg"
 
+import { IoMenu } from "react-icons/io5";
+import { RxCrossCircled } from "react-icons/rx";
+
 export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg">
@@ -17,19 +20,19 @@ export default function Navbar() {
                 <div >
                     <ul  id="menu">
                         <li ><a  href="./">Home</a></li>
-                        <li ><a  href="#">Our Fleet</a></li>
-                        <li ><a  href="service.html">Services</a>
+                        <li ><a  href="/ourfleet">Our Fleet</a></li>
+                        <li ><a  href="/services">Services</a>
                             <ul>
-                                <li ><a  href="service.html">Long Term CarHire</a></li>
-                                <li ><a  href="service-single.html">Chauffer Driven Service</a></li>
-                                <li ><a  href="service-single.html">Airport Transfer Service</a></li>
-                                <li ><a  href="service-single.html">Wedding Services</a></li>
+                                <li ><a  href="/services/longtermhire">Long Term CarHire</a></li>
+                                <li ><a  href="/services/chauffeurservice">Chauffer Driven Service</a></li>
+                                <li ><a  href="/services/airporttransfer">Airport Transfer Service</a></li>
+                                <li ><a  href="/services/weddingservices">Wedding Services</a></li>
                             </ul>
                         </li>
-                        <li ><a  href="about.html">Terms&Conditions</a></li>
-                        <li ><a  href="about.html">About Us</a></li>
-                        <li ><a  href="about.html">FAQS</a></li>
-                        <li ><a  href="contact.html">Contact Us</a></li>
+                        <li ><a  href="/terms&conditions">Terms&Conditions</a></li>
+                        <li ><a  href="/aboutus">About Us</a></li>
+                        <li ><a  href="/faqs">FAQS</a></li>
+                        <li ><a  href="/contactus">Contact Us</a></li>
                     </ul>
                 </div>
                 {/* <!-- Let’s Start Button Start --> */}
@@ -39,6 +42,32 @@ export default function Navbar() {
                 {/* <!-- Let’s Start Button End --> */}
             </div>
             {/* <!-- Main Menu End --> */}
+        </div>
+
+        {/* Mobile Menu */}
+        <div className="mobile-menu">
+            <div className="container">
+                <IoMenu  size={30}/>
+                <RxCrossCircled size={30}/>
+                <div className="mobile-menu-content">
+                    <ul id="mobile-menu">
+                        <li ><a  href="./">Home</a></li>
+                        <li ><a  href="/ourfleet">Our Fleet</a></li>
+                        <li ><a  href="/services">Services</a>
+                            <ul>
+                                <li ><a  href="/services/longtermhire">Long Term CarHire</a></li>
+                                <li ><a  href="/services/chauffeurservice">Chauffer Driven Service</a></li>
+                                <li ><a  href="/services/airporttransfer">Airport Transfer Service</a></li>
+                                <li ><a  href="/services/weddingservices">Wedding Services</a></li>
+                            </ul>
+                        </li>
+                        <li ><a  href="/terms&conditions">Terms&Conditions</a></li>
+                        <li ><a  href="/aboutus">About Us</a></li>
+                        <li ><a  href="/faqs">FAQS</a></li>
+                        <li ><a  href="/contactus">Contact Us</a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
     </nav>
   )
