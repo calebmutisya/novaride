8146237e38ad5838c9bd67e5a5061ee0ee8da27c
calebmutisya <mutisyacalebmusau@gmail.com>
2025-05-11 14:07:49 +0300

    animated mobile menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import Image from 'next/image'
 import logo from "../assets/images/logo.svg"
 
@@ -11,10 +11,22 @@ import MyButton from './MyButton';
 export default function Navbar() {
 
  const [open, setOpen] = useState(false)
+ const [shouldRender, setShouldRender] = useState(false);
+
+ useEffect(() => {
+    if (open) {
+      setShouldRender(true);
+    } else {
+      // Delay hiding until animation completes
+      const timeout = setTimeout(() => setShouldRender(false), 500); // match animation duration
+      return () => clearTimeout(timeout);
+    }
+  }, [open]);
 
   return (
-    <nav className=' text-myblack '>
-        <div className="container py-7.5  flex flex-row items-center justify-between">
+    <nav className=' text-myblack relative '>
+        {/* <!-- Main Menu Start --> */}
+        <div className="container z-2 py-7.5  flex flex-row items-center justify-between">
             {/* <!-- Logo Start --> */}
             <a href="./">
                 <Image src={logo} className='h-[26px]' alt="logo" />
@@ -22,7 +34,7 @@ export default function Navbar() {
             {/* <!-- Logo End --> */}
 
             
-            {/* <!-- Main Menu Start --> */}
+            
             <div className='flex flex-row justify-between'>
                 <ul  id="menu" className=' flex flex-row text-[16px]'>
                     <li ><a  href="./">Home</a></li>
@@ -41,37 +53,54 @@ export default function Navbar() {
                     <li ><a  href="/contactus">Contact Us</a></li>
                 </ul>
             </div>
-            {/* <!-- Main Menu End --> */}
+           
         
             <MyButton href="/contactus">Book A Rental</MyButton>
+
+            {
+                !open ?
+                <div className='p-[10px] bg-myorange hover:bg-myblack rounded-[8px] cursor-pointer' onClick={() => setOpen(true)}>
+                    <IoMenu  size={30} color='#FFF8F6' />
+                </div>
+                :
+                <div className='p-[10px] bg-myorange hover:bg-myblack  rounded-[8px] cursor-pointer' onClick={() => setOpen(false)}>
+                    <RxCrossCircled size={30} color='#FFF8F6' />
+                </div>
+            }
             
         </div>
+         {/* <!-- Main Menu End --> */}
 
         {/* Mobile Menu */}
-        <div className="mobile-menu container">
-            <div>
-                <IoMenu  size={30} color='#FF3600'/>
-                <RxCrossCircled size={30} color='#FF3600'/>
-                <div className="mobile-menu-content">
-                    <ul id="mobile-menu">
-                        <li ><a  href="./">Home</a></li>
-                        <li ><a  href="/ourfleet">Our Fleet</a></li>
-                        <li ><a  href="/services">Services</a>
-                            <ul >
-                                <li ><a  href="/services/longtermhire">Long Term CarHire</a></li>
-                                <li ><a  href="/services/chauffeurservice">Chauffer Driven Service</a></li>
-                                <li ><a  href="/services/airporttransfer">Airport Transfer Service</a></li>
-                                <li ><a  href="/services/weddingservices">Wedding Services</a></li>
-                            </ul>
-                        </li>
-                        <li ><a  href="/terms&conditions">Terms&Conditions</a></li>
-                        <li ><a  href="/aboutus">About Us</a></li>
-                        <li ><a  href="/faqs">FAQS</a></li>
-                        <li ><a  href="/contactus">Contact Us</a></li>
-                    </ul>
+        {
+            shouldRender &&
+            <div
+                className={`mobile-menu bg-myblack  text-mywhite w-full absolute top-[116px] left-0
+                    ${open ? 'animate-slide-down' : 'animate-slide-up'}
+                `}
+            >
+                <div>
+                    <div className="mobile-menu-content container pl-[15px] py-[15px]">
+                        <ul id="mobile-menu">
+                            <li ><a  href="./">Home</a></li>
+                            <li ><a  href="/ourfleet">Our Fleet</a></li>
+                            <li ><a  href="/services">Services</a>
+                                <ul >
+                                    <li ><a  href="/services/longtermhire">Long Term CarHire</a></li>
+                                    <li ><a  href="/services/chauffeurservice">Chauffer Driven Service</a></li>
+                                    <li ><a  href="/services/airporttransfer">Airport Transfer Service</a></li>
+                                    <li ><a  href="/services/weddingservices">Wedding Services</a></li>
+                                </ul>
+                            </li>
+                            <li ><a  href="/terms&conditions">Terms&Conditions</a></li>
+                            <li ><a  href="/aboutus">About Us</a></li>
+                            <li ><a  href="/faqs">FAQS</a></li>
+                            <li ><a  href="/contactus">Contact Us</a></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-        </div>
+        }
     </nav>
   )
 }
