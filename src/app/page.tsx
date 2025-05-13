ae5f8b0afe43159462766c05e303a6a66d3a0666
calebmutisya <mutisyacalebmusau@gmail.com>
2025-05-13 13:11:34 +0300

    edited

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,16 +6,16 @@ export default function Home() {
   return (
     <div >
       {/* Hero Start */}
-      <section className='h-[60vh] md:h-[40vh] lg:h-[50vh] xl:h-[92vh] relative text-mywhite'>
+      <section className='h-[70vh] md:h-[40vh] lg:h-[56vh] xl:h-[92vh] relative text-mywhite'>
         <Image src={herobg} alt="hero" className='w-full h-full object-cover' />
         <div className='absolute top-0 left-0 w-full h-full bg-myblack opacity-50'></div>
-        <div className=' absolute top-[20%] xl:top-[35%] w-full z-1 '>
+        <div className=' absolute top-[20%] xl:top-[30%] w-full z-1 '>
           <div className='w-[90vw] lg:w-[80vw] mx-auto  flex flex-col items-center'>
             <div className='text-[30px] lg:text-[42px] xl:text-[74px] text-center font-bold '>
             Your Trusted Car Rental Partner
             </div>
-            <div className='mt-[10px] text-[18px] text-center mb-[30px]'>
-              We’ve built a strong reputation for offering the best car rental services in Nairobi
+            <div className='mt-[10px] w-[90%] lg:w-[60%] text-[18px] text-center mb-[30px]'>
+            Whether you're planning a weekend getaway, a business trip, or just need a reliable ride for the day, we offer a wide range of vehicles to suit your needs.
             </div>
             <MyButton href={'/contactus'}>Make a Reservation</MyButton>
           </div>
